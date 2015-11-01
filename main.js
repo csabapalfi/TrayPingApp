37c99a3cb65200d5f63a0a4cb8edd7415dd5d496
Csaba Palfi <csaba@palfi.me>
2015-11-01 21:54:04 +0100

    refactor DNS lookup
    Signed-off-by: Csaba Palfi <csaba@palfi.me>

--- main.js
+++ main.js
@@ -1,42 +1,60 @@
+var timers = require('timers');
+var path = require('path');
+var dns = require('dns');
+var EventEmitter = require('events');
+
 var app = require('app');
 var Menu = require('menu');
 var Tray = require('tray');
-var ping = require('net-ping');
-var dns = require('dns');
-var timers = require('timers');
-var path = require('path');
+var dialog = require('dialog');
 
-var appIcon = null;
+var ping = require('net-ping');
+
 app.on('ready', function(){
   if (app.dock) app.dock.hide();
-  appIcon = new Tray(path.join(app.getAppPath(), 'icon.png'));
-  appIcon.setToolTip('ping google.com');
-  var contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Quit',
-      click: function() { app.quit(); }
-    },
-  ]);
-  appIcon.setContextMenu(contextMenu);
+
+  var trayIcon = new Tray(path.join(app.getAppPath(), 'icon.png'));
+
+  trayIcon.setContextMenu(Menu.buildFromTemplate([{
+    label: 'Quit',
+    click: function() { app.quit(); }
+  }]));
+
   var session = ping.createSession();
 
-  function pingHost(addresses) {
-    session.pingHost(addresses, function (error, target, sent, rcvd) {
+  var timeout = timers.setTimeout(dnsError, 2000);
+  dns.lookup('google.com', function onLookup(err, address) {
+    timers.clearTimeout(timeout);
+    if (err) return dnsError();
+    pingHost(address);
+  });
+
+  function pingHost(address) {
+    session.pingHost(address, function (error, target, sent, rcvd) {
       if (error) {
-        if (error instanceof ping.RequestTimedOutError) {
-            appIcon.setTitle('timeout');
-        } else {
-          appIcon.setTitle('error');
-        }
-      } else {
-        appIcon.setTitle(rcvd - sent + 'ms');
+        if(isTimeout(error)) return setTitle('timeout');
+        return setTitle('error');
       }
+      setTitle(rcvd - sent + 'ms');
+
     });
-    timers.setTimeout(pingHost, 2000, addresses);
+    timers.setTimeout(pingHost, 2000, address);
   }
 
-  dns.lookup('www.google.com', function onLookup(err, addresses) {
-    if (err) return appIcon.setTitle('error');
-    pingHost(addresses);
-  });
+  function dnsError() {
+    dialog.showErrorBox(
+      'DNS lookup error',
+      'Failed to resolve google.com'
+    );
+    app.quit();
+  }
+
+  function isTimeout(error) {
+    return error instanceof ping.RequestTimedOutError;
+  }
+
+  function setTitle(title) {
+    trayIcon.setTitle(title);
+  }
+
 });
