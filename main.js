b17560751b6bdd131cc1313ab97c6b0a5302fb8a
Csaba Palfi <csaba@palfi.me>
2015-11-01 17:23:14 +0100

    initial commit
    Signed-off-by: Csaba Palfi <csaba@palfi.me>

---
+++ main.js
@@ -0,0 +1,42 @@
+var app = require('app');
+var Menu = require('menu');
+var Tray = require('tray');
+var ping = require('net-ping');
+var dns = require('dns');
+var timers = require('timers');
+var path = require('path');
+
+var appIcon = null;
+app.on('ready', function(){
+  if (app.dock) app.dock.hide();
+  appIcon = new Tray(path.join(app.getAppPath(), 'icon.png'));
+  appIcon.setToolTip('ping google.com');
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Quit',
+      click: function() { app.quit(); }
+    },
+  ]);
+  appIcon.setContextMenu(contextMenu);
+  var session = ping.createSession();
+
+  function pingHost(addresses) {
+    session.pingHost(addresses, function (error, target, sent, rcvd) {
+      if (error) {
+        if (error instanceof ping.RequestTimedOutError) {
+            appIcon.setTitle('timeout');
+        } else {
+          appIcon.setTitle('error');
+        }
+      } else {
+        appIcon.setTitle(rcvd - sent + 'ms');
+      }
+    });
+    timers.setTimeout(pingHost, 2000, addresses);
+  }
+
+  dns.lookup('www.google.com', function onLookup(err, addresses) {
+    if (err) return appIcon.setTitle('error');
+    pingHost(addresses);
+  });
+});
