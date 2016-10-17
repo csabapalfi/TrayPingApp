7714acd58ee039f5b627d27c55568d02139919b3
Csaba Palfi <csaba@palfi.me>
2016-10-17 23:36:37 +0100

    update ignored files

--- Pods/Target Support Files/PlainPing/PlainPing-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <Cocoa/Cocoa.h>
-#endif
-
-#import "SimplePing.h"
-
-FOUNDATION_EXPORT double PlainPingVersionNumber;
-FOUNDATION_EXPORT const unsigned char PlainPingVersionString[];
-
