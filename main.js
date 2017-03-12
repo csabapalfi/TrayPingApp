a516c2332ec2053a9358fc8915132a00943304c4
Csaba Palfi <csaba@palfi.me>
2017-03-12 16:05:02 +0000

    fix network change + refactor - closes #2

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
-const timers = require('timers');
 const path = require('path');
-const dns = require('dns');
-const Ping = require('ping-lite');
-const { forever, timeout, parallel, waterfall } = require('async');
-const { app, dialog, BrowserWindow, Tray, Menu } = require('electron');
+const networkStatus = require('./network-status');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
+const timeoutMs = 2000;
 
-const dnsLatency = (host, callback) => {
-  const dnsStartMs = +(new Date);
-  dns.resolve(host, err => {
-    if (err) return callback(err);
-    callback(null, +(new Date) - dnsStartMs);
-  });
-}
-
-const pingLatency = (ip, callback) => {
-  const ping = new Ping(ip);
-  ping.send((err, latencyMs) => {
-    if (err) return callback(err);
-    callback(null, Math.round(latencyMs));
-  });
-}
-
-let mainWindow;
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({show: false});
+  new BrowserWindow({show: false});
   if (app.dock) app.dock.hide();
 
   const tray = new Tray(path.join(app.getAppPath(), 'icon.png'));
@@ -39,17 +20,12 @@ app.on('ready', () => {
   let title = `.../...ms`;
   tray.setTitle(title);
 
-  const refreshLatency = (check, arg, timeoutMs, titleRegex) =>
-    () => setInterval(() => {
-      timeout(check, timeoutMs)(arg, (err, latency) => {
-        title = title.replace(titleRegex, latency || '...');
-        tray.setTitle(title);
-      })
-    }, timeoutMs + 100);
-
-  parallel([
-    refreshLatency(dnsLatency, 'google.com', 2000, /[\d\.]+(?=\/)/),
-    refreshLatency(pingLatency, '8.8.8.8', 2000, /[\d\.]+(?=ms)/),
-  ]);
-
+  networkStatus({
+    timeoutMs: 2000,
+    intervalMs: 2000,
+    hostname: 'google.com',
+    address: '8.8.8.8'
+  }).on('latencies', ({dns, ping}) =>
+    tray.setTitle(`${dns || '...'}/${ping || '...'}ms`)
+  );
 });
